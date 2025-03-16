9bfd5b4246b870226012e8bca272cecfbee04b2e
lovelyme-nachi <ohakwemuna@gmail.com>
2025-03-16 23:56:25 +0100

    new modifications

--- assets/js/script.js
+++ assets/js/script.js
@@ -291,3 +291,28 @@ document.addEventListener("click", function (event) {
         closeTimeDropdown();
     }
 });
+
+
+// swiper animation styles
+new Swiper('.testimonies_wrapper', {
+    loop: true,
+    spaceBetween: 30,
+    // If we need pagination
+    pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true
+    },
+
+    breakpoints: {
+        0:{
+            slidesPerView: 1
+        },
+        768:{
+            slidesPerView: 2
+        },
+        1024:{
+            slidesPerView: 3
+        }
+    }
+});
