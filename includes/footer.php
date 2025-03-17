d1f82fd7586590fe3bc5f3e6217016dc52c97123
lovelyme-nachi <ohakwemuna@gmail.com>
2025-03-17 23:06:04 +0100

    renamed index.html to index.php

---
+++ includes/footer.php
@@ -0,0 +1,89 @@
+        <!-- footer -->
+        <footer>
+            <div class="footer_row">
+                <div class="footer_col">
+                    <a href="index.html" class="logo">Autorepair<span>.</span></a>
+                    <p>
+                        Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.
+                    </p>
+                    <div class="footer_icons">
+                        <a href="http://" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-facebook-f"></i></a>
+                        <a href="http://" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-x-twitter"></i></a>
+                        <a href="http://" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-instagram"></i></a>
+                        <a href="http://" target="_blank" rel="noopener noreferrer"><i class="fa-solid fa-basketball"></i></a>
+                    </div>
+                </div>
+                <div class="footer_col">
+                    <h3>services</h3>
+
+                    <ul>
+                        <li><a href="">
+                            <span><i class="fa-solid fa-check"></i></span>
+                            oil change
+                        </a></li>
+                        <li><a href="">
+                            <span><i class="fa-solid fa-check"></i></span>
+                            batteries
+                        </a></li>
+                        <li><a href="">
+                            <span><i class="fa-solid fa-check"></i></span>
+                            tow truck
+                        </a></li>
+                        <li><a href="">
+                            <span><i class="fa-solid fa-check"></i></span>
+                            tyre change
+                        </a></li>
+                        <li><a href="">
+                            <span><i class="fa-solid fa-check"></i></span>
+                            engine repair
+                        </a></li>
+                    </ul>
+                </div>
+                <div class="footer_col">
+                    <h3>contact information</h3>
+
+                    <ul>
+                        <li class="location">
+                            <span><i class="fa-solid fa-location-dot"></i></span>
+                            <p>203 Fake St. Mountain View, San Francisco, California, USA</p>
+                        </li>
+                        <li><a href="">
+                            <span><i class="fa-solid fa-phone"></i></span>
+                            +234 816 145 2508
+                        </a></li>
+                        <li class="email"><a href="">
+                            <span><i class="fa-solid fa-paper-plane"></i></span>
+                            info@youremail.com
+                        </a></li>
+                    </ul>
+                </div>
+                <div class="footer_col">
+                    <h3>business hours</h3>
+
+                    <div class="opening_days">
+                        <span>opening days</span>
+                        <p>Monday – Friday : 9am to 8pm</p>
+                        <p>Saturday : 9am to 5pm</p>
+                    </div>
+
+                    <div>
+                        <span>vacations</span>
+                        <p>All Sunday Days</p>
+                        <p>All Official Holidays</p>
+                    </div>
+                </div>
+            </div>
+
+            <p class="footer_note">Copyright &copy;<span class="footer_date"></span>. All rights reserved. <a href="">Cookies & Policy.</a></p>
+        </footer>
+        <!-- footer end -->
+    </main>
+
+    <!-- swiper js -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+    <!-- ionicons -->
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Auto Repair</title>
+
+    <!-- css -->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!-- js -->
+    <script src="assets/js/script.js" defer></script>
+
+    <!-- swiper css -->
+    <link rel="stylesheet"href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+
+    <!-- favicon -->
+
+    <!-- google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Alegreya+Sans+SC:ital,wght@0,100;0,300;0,400;0,500;0,700;0,800;0,900;1,100;1,300;1,400;1,500;1,700;1,800;1,900&family=Allison&family=Bebas+Neue&family=Cedarville+Cursive&family=Dekko&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Jost:ital,wght@0,100..900;1,100..900&family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&family=Lora:ital,wght@0,400..700;1,400..700&family=Merriweather:ital,opsz,wght@0,18..144,300..900;1,18..144,300..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+
+    <!-- fontawesome -->
+    <script src="https://kit.fontawesome.com/ee0082ad61.js" crossorigin="anonymous"></script>
+</head>
+<body>
+
+    <!-- main container -->
+    <main class="main_container">
+        <!-- aside -->
+        <aside class="aside_content">
+            <div class="aside_col">
+                <a href="index.html" class="logo">Autorepair<span>.</span></a>
+            </div>
+            <div class="aside_col">
+                <div class="aside_info">
+                    <span><i class="fa-solid fa-paper-plane"></i></span>
+                    <div>
+                        <p>Address</p>
+                        <p>721 New York NY 100016</p>
+                    </div>
+                </div>
+                <div class="aside_info">
+                    <span><i class="fa-solid fa-paper-plane"></i></span>
+                    <div>
+                        <p>call us</p>
+                        <p>+234816 145 2508</p>
+                    </div>
+                </div>
+                <div class="aside_info">
+                    <a href="http://" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-facebook-f"></i></a>
+                    <a href="http://" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-x-twitter"></i></a>
+                    <a href="http://" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="http://" target="_blank" rel="noopener noreferrer"><i class="fa-solid fa-basketball"></i></a>
+                </div>
+            </div>
+        </aside>
+        <!-- aside end -->
+
+        <!-- header -->
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="">home</a></li>
+                    <li><a href="">about</a></li>
+                    <li><a href="">services</a></li>
+                    <li><a href="">products</a></li>
+                    <li><a href="">contact</a></li>
+                    <li><a href="">sign up</a></li>
+                </ul>
+            </nav>
+            <div class="search_box">
+                <input type="text" name="" id="" placeholder="Search">
+                <span><i class="fa-solid fa-magnifying-glass"></i></span>
+            </div>
+
+            <div class="show_menu"><span><i class="fa-solid fa-bars"></i></span> Menu</div>
+        </header>
+        <!-- header end -->
+
+        <!-- small_screen_nav -->
+        <div class="small_screen_nav">
+            <div class="small_screen_logo_container">
+                <a href="index.html" class="small_screen_logo">Autorepair<span>.</span></a>
+                <span class="close_small_screen_nav"><i class="fa-solid fa-xmark"></i></span>
+            </div>
+
+            <ul>
+                <li><a href="">home <span><i class="fa-solid fa-angle-right"></i></span></a></li>
+                <li><a href="">about <span><i class="fa-solid fa-angle-right"></i></span></a></li>
+                <li><a href="">services <span><i class="fa-solid fa-angle-right"></i></span></a></li>
+                <li><a href="">products <span><i class="fa-solid fa-angle-right"></i></span></a></li>
+                <li><a href="">contact <span><i class="fa-solid fa-angle-right"></i></span></a></li>
+                <li><a href="">sign up <span><i class="fa-solid fa-angle-right"></i></span></a></li>
+            </ul>
+
+            <div class="small_screen_location">
+                <h4>our office</h4>
+                <p>721 New York NY 100016.</p>
+                <p>Phone: 234 816 145 2508</p>
+                <p>Email: <a href="http://" target="_blank" rel="noopener noreferrer">ohakwemuna@gmail.com</a></p>
+
+                <div class="small_screen_icons">
+                    <a href="http://" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-x-twitter"></i></a>
+                    <a href="http://" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-linkedin"></i></a>
+                    <a href="http://" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-facebook-messenger"></i></a>
+                    <a href="http://" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-instagram"></i></a>
+                </div>
+            </div>
+        </div>
+        <!-- small_screen_nav -->
